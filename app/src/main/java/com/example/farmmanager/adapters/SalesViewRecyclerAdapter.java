4a4f2c8027f9441db0d78a8965607f88dbb928ea
Erick-Wanjohi <wanjohierick07@gmail.com>
2021-07-25 12:25:09 +0300

    view Sales

--- app/src/main/java/com/example/farmmanager/adapters/SalesViewRecyclerAdapter.java
+++ app/src/main/java/com/example/farmmanager/adapters/SalesViewRecyclerAdapter.java
@@ -1,10 +1,14 @@
 package com.example.farmmanager.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.farmmanager.R;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -19,23 +23,25 @@ public class SalesViewRecyclerAdapter extends RecyclerView.Adapter<SalesViewRecy
     @NotNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_row, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull MyViewHolder holder, int position) {
-
+        holder.textView.setText(records.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return records.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-
+        TextView textView;
         public MyViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            textView = itemView.findViewById(R.id.customRow);
         }
     }
 }
--- app/src/main/java/com/example/farmmanager/ui/sales/SalesView.java
+++ app/src/main/java/com/example/farmmanager/ui/sales/SalesView.java
@@ -1,6 +1,7 @@
 package com.example.farmmanager.ui.sales;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +11,17 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.farmmanager.R;
 import com.example.farmmanager.adapters.SalesViewRecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SalesView extends Fragment {
 
@@ -22,6 +29,7 @@ public class SalesView extends Fragment {
     RecyclerView salesRecycler;
     SalesViewRecyclerAdapter salesViewRecyclerAdapter;
     List<String> records = new ArrayList<>();
+    private static final String TAG = "SalesView";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,11 +37,28 @@ public class SalesView extends Fragment {
         salesRecycler = view.findViewById(R.id.recorded_sales_recycler);
         records.add("Record 1");
         records.add("record 2");
+        getRecordsFromDatabase();
         salesRecycler.setHasFixedSize(true);
         salesRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
         salesViewRecyclerAdapter = new SalesViewRecyclerAdapter(records);
         salesRecycler.setAdapter(salesViewRecyclerAdapter);
 
         return view;
+    }
+
+    private void getRecordsFromDatabase() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, fetchRecordsUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "onResponse: "+ response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: "+ error.getLocalizedMessage()  );
+            }
+        });
+        stringRequest.setShouldCache(false);
+        Volley.newRequestQueue(requireContext()).add(stringRequest);
     }
 }
